b42da49f8c1b793c7c9990b34ebb03743d129835
jarvemets <32923790+jarvemets@users.noreply.github.com>
2017-10-26 19:24:15 +0300

    Add files via upload

---
+++ 07-while/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool hasNotGuessed = true;
+            while (hasNotGuessed)
+            {
+                if (number == number1)
+                {
+                    hasNotGuessed = false;
+                }
+            }
+        }
+    }
+}
