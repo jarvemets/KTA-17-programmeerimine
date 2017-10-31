1e46d081323307835cde7f3ec322dc9c196e6130
jarvemets <32923790+jarvemets@users.noreply.github.com>
2017-10-31 21:30:11 +0200

    Add files via upload

---
+++ 02-exercise/02-exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Mina olen pikkuse spetsialist.");
+            Console.WriteLine();
+
+            float height;
+
+            Console.WriteLine("Kirjuta mulle oma pikkus cm \n");
+
+            height = int.Parse(Console.ReadLine());
+
+            if (height < 150.0)
+
+            Console.WriteLine("Päkapikk! \n");
+
+            else if ((height >= 150.0) && (height <= 170.0))
+
+            Console.WriteLine("Täitsa tavaline \n");
+
+            else if ((height >= 170.0) && (height <= 195.0))
+
+            Console.WriteLine("Täitsa pikk oled! \n");
+
+            else
+            Console.WriteLine("Einoh, ebanormaalselt pikk \n");
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
