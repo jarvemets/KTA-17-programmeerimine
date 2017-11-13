b879bc3df5622b4fdcd786955530fe5ba636a7ba
jarvemets <32923790+jarvemets@users.noreply.github.com>
2017-11-13 12:28:38 +0200

    liigutasin õigesse kohta

---
+++ 01 exercise/05-parse-input-multiply-divide/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_parse_input_multiply_divide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen kalkulaatori programm.");
+            Console.WriteLine("Ütle mulle 2 numbrit ja ma ütlen Sulle nende korrutise ja jagatise");
+            Console.WriteLine();
+
+            Console.Write("Esimene number: ");
+            string input1 = Console.ReadLine();
+
+            Console.Write("Teine number: ");
+            string input2 = Console.ReadLine();
+
+            int number1 = int.Parse(input1);
+            int number2 = int.Parse(input2);
+            int tulemus1 = number1 * number2;
+            int tulemus2 = number1 / number2;
+
+            Console.WriteLine("Korrutades " + input1 + " ja " + input2 + " on tulemuseks: " + tulemus1);
+            Console.WriteLine("Jagades " + input1 + " ja " + input2 + " on tulemuseks: " + tulemus2);
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02 exercise/02-arvasid-ara/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_arvasid_ara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen kalkulaatori programm. Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata");
+            Console.WriteLine();
+            Console.Write("Sinu number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            int magicNumber = 55;
+            if (magicNumber > number)
+            {
+                Console.WriteLine("Sinu number on väiksem");
+            }
+
+            if (magicNumber < number)
+            {
+                Console.WriteLine("Sinu number on suurem");
+            }
+
+            if (magicNumber == number)
+            {
+                Console.WriteLine("Arvasid ära!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
